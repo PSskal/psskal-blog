254b04acb2790b5a2a9227370d0217dee0930506
Richard Pascual <ricpascual@gmail.com>
2023-02-28 13:16:43 -0500

    feat: :zap: coding my personal website
    use astro, tailwind, chacra ui, nextjs

---
+++ pages/works.js
@@ -0,0 +1,122 @@
+import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import Layout from '../components/layouts/article'
+import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
+
+import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
+import thumbNetflix from '../public/images/works/netflix.png'
+
+import thumbTodoApp from '../public/images/works/todoapp.png'
+import thumbMenkiki from '../public/images/works/menkiki_eyecatch.png'
+import thumbMargelo from '../public/images/works/margelo_eyecatch.png'
+import thumbModeTokyo from '../public/images/works/modetokyo_eyecatch.png'
+import thumbStyly from '../public/images/works/styly_eyecatch.png'
+
+const Works = () => (
+  <Layout title="Works">
+    <Container>
+      <Heading as="h3" fontSize={20} mb={4}>
+        Works
+      </Heading>
+
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <Section>
+          <WorkGridItem id="inkdrop" title="Inkdrop" thumbnail={thumbInkdrop}>
+            A Markdown note-taking app with 100+ plugins, cross-platform and
+            encrypted data sync support
+          </WorkGridItem>
+        </Section>
+        <Section>
+          <WorkGridItem
+            id="netflix_clone"
+            title="Netflix Case Study"
+            thumbnail={thumbNetflix}
+          >
+            Netflix Clone app use React, Tailwind
+          </WorkGridItem>
+        </Section>
+
+        <Section delay={0.1}>
+          <WorkGridItem
+            id="fourpainters"
+            title="The TodoAPP"
+            thumbnail={thumbTodoApp}
+          >
+            an application for the tasks that will be executed in the machine
+          </WorkGridItem>
+        </Section>
+        <Section delay={0.1}>
+          <WorkGridItem id="menkiki" thumbnail={thumbMenkiki} title="Menkiki">
+            An app that suggests ramen(noodle) shops based on a given photo of
+            the ramen you want to eat
+          </WorkGridItem>
+        </Section>
+      </SimpleGrid>
+
+      <Section delay={0.2}>
+        <Divider my={6} />
+
+        <Heading as="h3" fontSize={20} mb={4}>
+          Collaborations
+        </Heading>
+      </Section>
+
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <Section delay={0.3}>
+          <WorkGridItem id="margelo" thumbnail={thumbMargelo} title="Margelo">
+            A website of the elite app development and contracting agency based
+            in Austria
+          </WorkGridItem>
+        </Section>
+        <Section delay={0.3}>
+          <WorkGridItem
+            id="modetokyo"
+            thumbnail={thumbModeTokyo}
+            title="mode.tokyo"
+          >
+            The mode magazine for understanding to personally enjoy Japan
+          </WorkGridItem>
+        </Section>
+        <Section delay={0.3}>
+          <WorkGridItem id="styly" thumbnail={thumbStyly} title="Styly">
+            A VR Creative tools for fashion brands
+          </WorkGridItem>
+        </Section>
+      </SimpleGrid>
+
+      {/* <Section delay={0.4}>
+        <Divider my={6} />
+
+        <Heading as="h3" fontSize={20} mb={4}>
+          Old works
+        </Heading>
+      </Section>
+
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <Section delay={0.5}>
+          <WorkGridItem id="pichu2" thumbnail={thumbPichu2} title="Pichu*Pichu">
+            Twitter client app for iPhone Safari
+          </WorkGridItem>
+        </Section>
+        <Section delay={0.5}>
+          <WorkGridItem
+            id="freedbtagger"
+            thumbnail={thumbFreeDBTagger}
+            title="freeDBTagger"
+          >
+            Automatic audio file tagging tool using FreeDB for Windows
+          </WorkGridItem>
+        </Section>
+        <Section delay={0.6}>
+          <WorkGridItem id="amembo" thumbnail={thumbAmembo} title="Amembo">
+            P2P private file sharing tool with MSN Messenger integration for
+            Windows
+          </WorkGridItem>
+        </Section>
+      </SimpleGrid> */}
+    </Container>
+  </Layout>
+)
+
+export default Works
+export { getServerSideProps } from '../components/chakra'
