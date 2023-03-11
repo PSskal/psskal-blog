88c95d9bc4412048a65e76e6465046974eb765b6
Richard Pascual <ricpascual@gmail.com>
2023-03-10 23:18:22 -0500

    in the web

---
+++ .next/server/pages/works.js
@@ -0,0 +1,319 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 547;
+exports.ids = [547];
+exports.modules = {
+
+/***/ 8281:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/margelo_eyecatch.3f6e57f4.png","height":400,"width":720,"blurDataURL":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAECAIAAAA8r+mnAAAAb0lEQVR42gFkAJv/ALu7u7/AwMLCwrS1toeKjWFlaoqMjp2dngCKjI5scXaAg4atrq/Gx8exsbKio6WfoKEAv7+/hYiLY2hucnZ6mpyerq6unZ6fra2tAMnJydTU1LGys4KFiGJnbKKjpH5+f31+f6CSOpoWykkKAAAAAElFTkSuQmCC","blurWidth":8,"blurHeight":4});
+
+/***/ }),
+
+/***/ 3054:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/netflix.75255502.png","height":400,"width":720,"blurDataURL":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAECAIAAAA8r+mnAAAAbElEQVR42mMItnX08A7wDY2KS83wcXd2M1D09/fOyE5j6Oqb0NTWmVtUXlNbV5ERlxvuERvoVVaUw1BZ35SeWxASGZEUGxkTGpCWHBcaG+cVmcRg6+Hj5uURHumT5qaWE2TfV5ZU2zs5u38hAOSgJDHI7hICAAAAAElFTkSuQmCC","blurWidth":8,"blurHeight":4});
+
+/***/ }),
+
+/***/ 2593:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/todoapp.15b9c2a4.png","height":400,"width":720,"blurDataURL":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAECAIAAAA8r+mnAAAAb0lEQVR42gFkAJv/ABuUqgCOpVyhs+bq7fj7/KDBzACHoB+TqQAXk6gAjaRhpbb++vn///qmx88Ah6AymKwAFpGnAI2kV5+ytdPsudj2g7LGAIWceK64ACdtiQBlg1uDmaPJ6KDP9H6ftgBYd1mQoDV2OBMJDxyZAAAAAElFTkSuQmCC","blurWidth":8,"blurHeight":4});
+
+/***/ }),
+
+/***/ 3489:
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.a(module, async (__webpack_handle_async_dependencies__, __webpack_async_result__) => { try {
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__),
+/* harmony export */   "getServerSideProps": () => (/* reexport safe */ _components_chakra__WEBPACK_IMPORTED_MODULE_9__.N)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _chakra_ui_react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2210);
+/* harmony import */ var _components_layouts_article__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(951);
+/* harmony import */ var _components_section__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(2112);
+/* harmony import */ var _components_grid_item__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(8598);
+/* harmony import */ var _public_images_works_netflix_png__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(3054);
+/* harmony import */ var _public_images_works_todoapp_png__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(2593);
+/* harmony import */ var _public_images_works_margelo_eyecatch_png__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(8281);
+/* harmony import */ var _public_images_works_learning_png__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(195);
+/* harmony import */ var _components_chakra__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(7020);
+var __webpack_async_dependencies__ = __webpack_handle_async_dependencies__([_chakra_ui_react__WEBPACK_IMPORTED_MODULE_1__, _components_layouts_article__WEBPACK_IMPORTED_MODULE_2__, _components_section__WEBPACK_IMPORTED_MODULE_3__, _components_grid_item__WEBPACK_IMPORTED_MODULE_4__, _components_chakra__WEBPACK_IMPORTED_MODULE_9__]);
+([_chakra_ui_react__WEBPACK_IMPORTED_MODULE_1__, _components_layouts_article__WEBPACK_IMPORTED_MODULE_2__, _components_section__WEBPACK_IMPORTED_MODULE_3__, _components_grid_item__WEBPACK_IMPORTED_MODULE_4__, _components_chakra__WEBPACK_IMPORTED_MODULE_9__] = __webpack_async_dependencies__.then ? (await __webpack_async_dependencies__)() : __webpack_async_dependencies__);
+
+
+
+
+
+
+
+
+
+const Works = ()=>/*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_layouts_article__WEBPACK_IMPORTED_MODULE_2__/* ["default"] */ .Z, {
+        title: "Works",
+        children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_1__.Container, {
+            children: [
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_1__.Heading, {
+                    as: "h3",
+                    fontSize: 20,
+                    mb: 4,
+                    children: "Works"
+                }),
+                /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(_chakra_ui_react__WEBPACK_IMPORTED_MODULE_1__.SimpleGrid, {
+                    columns: [
+                        1,
+                        1,
+                        2
+                    ],
+                    gap: 6,
+                    children: [
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_section__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .Z, {
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_grid_item__WEBPACK_IMPORTED_MODULE_4__/* .WorkGridItem */ .Et, {
+                                id: "csmer",
+                                title: "Csmer",
+                                thumbnail: _public_images_works_learning_png__WEBPACK_IMPORTED_MODULE_8__/* ["default"] */ .Z,
+                                children: "a courses plataform for studentes who finished the high school"
+                            })
+                        }),
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_section__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .Z, {
+                            delay: 0.1,
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_grid_item__WEBPACK_IMPORTED_MODULE_4__/* .WorkGridItem */ .Et, {
+                                id: "menkiki",
+                                thumbnail: _public_images_works_margelo_eyecatch_png__WEBPACK_IMPORTED_MODULE_7__/* ["default"] */ .Z,
+                                title: "Margelo",
+                                children: "A website of the elite app development and contracting agency"
+                            })
+                        }),
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_section__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .Z, {
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_grid_item__WEBPACK_IMPORTED_MODULE_4__/* .WorkGridItem */ .Et, {
+                                id: "netflix",
+                                title: "Netflix",
+                                thumbnail: _public_images_works_netflix_png__WEBPACK_IMPORTED_MODULE_5__/* ["default"] */ .Z,
+                                children: "Netflix Clone app use React, Tailwind"
+                            })
+                        }),
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_section__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .Z, {
+                            delay: 0.1,
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_grid_item__WEBPACK_IMPORTED_MODULE_4__/* .WorkGridItem */ .Et, {
+                                id: "fourpainters",
+                                title: "The TodoAPP",
+                                thumbnail: _public_images_works_todoapp_png__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Z,
+                                children: "an application for the tasks that will be executed in the machine"
+                            })
+                        })
+                    ]
+                })
+            ]
+        })
+    });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Works);
+
+
+__webpack_async_result__();
+} catch(e) { __webpack_async_result__(e); } });
+
+/***/ }),
+
+/***/ 2805:
+/***/ ((module) => {
+
+module.exports = require("@emotion/react");
+
+/***/ }),
+
+/***/ 3918:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/amp-context.js");
+
+/***/ }),
+
+/***/ 5732:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/amp-mode.js");
+
+/***/ }),
+
+/***/ 3280:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/app-router-context.js");
+
+/***/ }),
+
+/***/ 2796:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/head-manager-context.js");
+
+/***/ }),
+
+/***/ 4486:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/image-blur-svg.js");
+
+/***/ }),
+
+/***/ 744:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/image-config-context.js");
+
+/***/ }),
+
+/***/ 5843:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/image-config.js");
+
+/***/ }),
+
+/***/ 9552:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/image-loader");
+
+/***/ }),
+
+/***/ 4964:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router-context.js");
+
+/***/ }),
+
+/***/ 1751:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/add-path-prefix.js");
+
+/***/ }),
+
+/***/ 3938:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/format-url.js");
+
+/***/ }),
+
+/***/ 1109:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/is-local-url.js");
+
+/***/ }),
+
+/***/ 8854:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/parse-path.js");
+
+/***/ }),
+
+/***/ 3297:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/remove-trailing-slash.js");
+
+/***/ }),
+
+/***/ 7782:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/resolve-href.js");
+
+/***/ }),
+
+/***/ 2470:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/side-effect.js");
+
+/***/ }),
+
+/***/ 9232:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/utils.js");
+
+/***/ }),
+
+/***/ 618:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/utils/warn-once.js");
+
+/***/ }),
+
+/***/ 968:
+/***/ ((module) => {
+
+module.exports = require("next/head");
+
+/***/ }),
+
+/***/ 6689:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ }),
+
+/***/ 2210:
+/***/ ((module) => {
+
+module.exports = import("@chakra-ui/react");;
+
+/***/ }),
+
+/***/ 149:
+/***/ ((module) => {
+
+module.exports = import("@chakra-ui/theme-tools");;
+
+/***/ }),
+
+/***/ 6197:
+/***/ ((module) => {
+
+module.exports = import("framer-motion");;
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [664,121,675,655,20,951,34], () => (__webpack_exec__(3489)));
+module.exports = __webpack_exports__;
+
+})();
